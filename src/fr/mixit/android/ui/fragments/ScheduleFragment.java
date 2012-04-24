87daae31a8f19ca991151e99c2bc6958f1c0628c
Mathieu Calba <mathieu.calba@gmail.com>
2012-04-24 13:27:39 +0200

    adding schedule (image) and adding correct image for action bar icon

--- src/fr/mixit/android/ui/fragments/ScheduleFragment.java
+++ src/fr/mixit/android/ui/fragments/ScheduleFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings.ZoomDensity;
+import android.webkit.WebView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
@@ -11,9 +13,22 @@ import fr.mixit.android.R;
 
 public class ScheduleFragment extends SherlockFragment {
 
+	WebView mJpegView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+        mJpegView = (WebView) v.findViewById(R.id.jpg_webview);
+        mJpegView.getSettings().setBuiltInZoomControls(true);
+        mJpegView.getSettings().setDefaultZoom(ZoomDensity.FAR);
 		return v;
 	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		mJpegView.loadUrl("file:///android_asset/programme_site.html");
+	}
+	
 }
